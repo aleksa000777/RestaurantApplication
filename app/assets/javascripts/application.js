fdf83179bf416258d850ff68ae370c5e39242e88
Aleksandra Matiyev <aleksa000777@mail.ru>
2015-12-25 13:54:23 -0500

    euro to dollar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,10 +27,6 @@ function initPage() {
   bill();
 }
 
-// $(document).ready(function(){
-//   myFunction();
-//   bill();
-// })
 var myVar;
 function myFunction() {
     myVar = setInterval(init, 3000);
